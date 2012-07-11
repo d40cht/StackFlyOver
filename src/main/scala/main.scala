dadd3d2d3e824af3c9cecc18a1018bb348db3900
Alex Wilson <alex_wilson@pobox.com>
2012-07-11 20:59:31 +0100

    Polling for questions.

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,99 +1,73 @@
 import dispatch._
-import net.liftweb.json.{JsonParser}
+import net.liftweb.json.{JsonParser, DefaultFormats}
 
+
+object Dispatch
+{
+    lazy val h = new Http
+    
+    def pullJSON( urlStr : String ) =
+    {
+        val u = url( urlStr )
+        val res = h(u as_str)   
+        JsonParser.parse(res)
+    }
+}
+
+case class User(
+    val user_id : String,
+    val display_name : String,
+    val reputation : Long )
+
+case class Question(
+    val title : String,
+    val creation_date : Long,
+    val score : Int,
+    val question_id  : Long,
+    val answer_count : Int,
+    val tags : List[String],
+    val link : String,
+    val owner : User )
+    
+class QuestionScraper
+{
+    private var questionMap = Map[Long, Question]()
+    
+    def run()
+    {
+        while ( true )
+        {
+            implicit val formats = DefaultFormats
+            
+            val json = Dispatch.pullJSON( "http://api.stackexchange.com/2.0/questions?site=stackoverflow&pagesize=100" )
+            val items = (json \ "items").children
+            
+            val questions = items.map( _.extract[Question] )
+            
+            println( "Tick" )
+            for ( q <- questions )
+            {
+                if ( !(questionMap contains q.question_id) )
+                {
+                    println( q.title )
+                }
+                questionMap += (q.question_id -> q)
+            }
+            
+            val backoff = json \ "backoff"
+            println( "Backoff: " + backoff )
+            Thread.sleep(10 * 1000)
+        }
+    }
+}
 
 object Main extends App
 {
     override def main( args : Array[String] ) =
     {
-        /*val res = Json.parse[Map[String, Int]]("""{"one":1,"two":2}""")*/
-        
-        {
-            val h = new Http
+        val qs = new QuestionScraper()
             
-            val u = url("http://api.stackexchange.com/2.0/users/1;2;3;4;5;6;7;8;9;10?order=desc&sort=reputation&site=stackoverflow")
-            val res = h(u as_str)
-            
-            val json = JsonParser.parse(res)
-            
-            val items = (json \ "items").children
-            
-            for ( user <- items )
-            {
-                println( user \ "display_name" )
-                println( "  " + user \ "reputation" )
-                println( "  " + user \ "location" )
-            }
-            
-            // Geocode using the google api (max 15000 a day): https://developers.google.com/maps/documentation/geocoding/
-            // http://maps.googleapis.com/maps/api/geocode/json?address=San+Diego,CA&sensor=false
-            //
-            // and p
-            
-            //println( res )
-            //println( json.toString )
-        }
-        
-        //println( "Hello world ", res )
+        qs.run()
     }
 }
 
-/*
-{
-   "results" : [
-      {
-         "address_components" : [
-            {
-               "long_name" : "San Diego",
-               "short_name" : "San Diego",
-               "types" : [ "locality", "political" ]
-            },
-            {
-               "long_name" : "San Diego",
-               "short_name" : "San Diego",
-               "types" : [ "administrative_area_level_2", "political" ]
-            },
-            {
-               "long_name" : "California",
-               "short_name" : "CA",
-               "types" : [ "administrative_area_level_1", "political" ]
-            },
-            {
-               "long_name" : "United States",
-               "short_name" : "US",
-               "types" : [ "country", "political" ]
-            }
-         ],
-         "formatted_address" : "San Diego, CA, USA",
-         "geometry" : {
-            "bounds" : {
-               "northeast" : {
-                  "lat" : 33.1142490,
-                  "lng" : -116.908160
-               },
-               "southwest" : {
-                  "lat" : 32.5348560,
-                  "lng" : -117.28216650
-               }
-            },
-            "location" : {
-               "lat" : 32.71532920,
-               "lng" : -117.15725510
-            },
-            "location_type" : "APPROXIMATE",
-            "viewport" : {
-               "northeast" : {
-                  "lat" : 32.86540990,
-                  "lng" : -116.90113630
-               },
-               "southwest" : {
-                  "lat" : 32.56499550,
-                  "lng" : -117.41337390
-               }
-            }
-         },
-         "types" : [ "locality", "political" ]
-      }
-   ],
-   "status" : "OK"
-*/
