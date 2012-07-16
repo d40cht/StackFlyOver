84dad51c0f8bc96918fe9cf2a3f6e73f73f9a2fe
Alex Wilson <alex_wilson@pobox.com>
2012-07-16 21:19:19 +0100

    Added in initial dependency on scalaquery and h2.

--- playsite/app/controllers/Application.scala
+++ playsite/app/controllers/Application.scala
@@ -3,12 +3,23 @@ package controllers
 import play.api._
 import play.api.mvc._
 
+import org.scalaquery.session.Database
+import org.scalaquery.session._
+import org.scalaquery.session.Database.threadLocalSession
+import org.scalaquery.ql.basic.BasicDriver.Implicit._
+
+
 object Application extends Controller
 {
     val googleMapsKey = "AIzaSyA_F10Lcod9fDputQVMZOtM4cMMaFbJybU"
     def index = Action
     {
-        Ok(views.html.index("Your new application is ready."))
+        val db = Database.forURL("jdbc:h2:file:/home/alex/Devel/AW/CriticalMass/stack_users;DB_CLOSE_DELAY=-1", driver = "org.h2.Driver")
+        
+        db withSession
+        {
+            Ok(views.html.index("Your new application is ready."))
+        }
     }
   
 }
--- playsite/project/Build.scala
+++ playsite/project/Build.scala
@@ -8,11 +8,14 @@ object ApplicationBuild extends Build {
     val appVersion      = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(
-      // Add your project dependencies here,
+        "com.h2database" % "h2" % "1.3.167",
+        "org.scalaquery" % "scalaquery_2.9.1" % "0.10.0-M1"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
       // Add your own project settings here      
+      
+        
     )
 
 }
