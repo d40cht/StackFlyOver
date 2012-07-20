97c84f75c99fff432191252cfca5040756895b83
Alex Wilson <alex_wilson@pobox.com>
2012-07-20 17:47:35 +0100

    Various fiddles with presentation.

--- playsite/project/Build.scala
+++ playsite/project/Build.scala
@@ -4,7 +4,7 @@ import PlayProject._
 
 object ApplicationBuild extends Build {
 
-    val appName         = "CriticalMass"
+    val appName         = "StackFlyOver"
     val appVersion      = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(
---
+++ playsite/runserver.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Install authbind to enable user bind to port 80
+#
+# sudo touch /etc/authbind/byport/80
+# sudo chown alex:alex /etc/authbind/byport/80
+# chmod 755 /etc/authbind/byport/80
+#
+# Then: ../../play-2.0.2/play stage
+# generates: ./target/start
+#
+set -e
+../../play-2.0.2/play stage
+cd target
+authbind --deep java -Djava.net.preferIPv4Stack=true -DLD_PRELOAD=/usr/lib/authbind/libauthbind.so.1 -Dhttp.port=80 -cp "`dirname $0`/staged/*" play.core.server.NettyServer `dirname $0`/..
