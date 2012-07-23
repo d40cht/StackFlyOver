2f485ffd7867e5c745ad0b57386cb71af188dbe0
Alex Wilson <alex_wilson@pobox.com>
2012-07-23 18:54:05 +0100

    Iteration.

--- playsite/public/javascripts/common.js
+++ playsite/public/javascripts/common.js
@@ -9,8 +9,7 @@
             table.fnClearTable(this);
 
             for (var i=0; i<json.aaData.length; i++) {
-                var ins = ((json.aaData.length)-1)-i;
-                table.oApi._fnAddData(oSettings, json.aaData[ins]);
+                table.oApi._fnAddData(oSettings, json.aaData[i]);
             }
 
             oSettings.aiDisplay = oSettings.aiDisplayMaster.slice();
