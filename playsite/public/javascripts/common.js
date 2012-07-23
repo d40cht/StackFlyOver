6067d134228705d890e1b8dc0b41be87ab303da2
Alex Wilson <alex_wilson@pobox.com>
2012-07-23 13:08:17 +0100

    Iteration.

--- playsite/public/javascripts/common.js
+++ playsite/public/javascripts/common.js
@@ -126,29 +126,30 @@
                             google.maps.event.addListener(smarker, 'click', function() {
                               infowindow.open(map,smarker);
                               
-                            // Reload the table with local users
-                            if ( oTable == null )
-                            {
-                                oTable = $('#example').dataTable( {
-                                    "bProcessing": false,
-                                    "bAutoWidth":true,
-                                    "bFilter":false,
-                                    "bInfo":false,
-                                    "bLengthChange":false,
-                                    "sAjaxSource": "/markerUsers?dh_id=" + item.dh_id,
-                                    "aoColumns": [
-                                        { "mDataProp": "reputation" },
-                                        { "mDataProp": "user_name" },
-                                        { "mDataProp": "location" }
-                                    ]
-                                } );
+                                // Reload the table with local users
+                                if ( oTable == null )
+                                {
+                                    oTable = $('#example').dataTable( {
+                                        "bProcessing": false,
+                                        "bAutoWidth":true,
+                                        "bFilter":false,
+                                        "bInfo":false,
+                                        "bLengthChange":false,
+                                        "sAjaxSource": "/markerUsers?dh_id=" + item.dh_id,
+                                        "aoColumns": [
+                                            { "mDataProp": "reputation" },
+                                            { "mDataProp": "user_name" },
+                                            { "mDataProp": "location" }
+                                        ]
+                                    } );
+                                }
+                                else
+                                {
+                                    refreshTable( oTable, "/markerUsers?dh_id=" + item.dh_id );
+                                }
+                                
+                                markersArray.push(smarker);
                             }
-                            else
-                            {
-                                refreshTable( oTable, "/markerUsers?dh_id=" + item.dh_id );
-                            }
-                            
-                            markersArray.push(smarker);
                         }
                         
                     } );
