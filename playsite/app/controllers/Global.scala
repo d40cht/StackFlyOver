630d68a0ce554a614464adaa6fc8f78c21125c37
Alex Wilson <alex.wilson@navetas.com>
2013-02-10 19:11:42 +0000

    Add in play-Quartz plugin.

--- playsite/app/controllers/Global.scala
+++ playsite/app/controllers/Global.scala
@@ -6,22 +6,34 @@ import org.scalaquery.session.Database.threadLocalSession
 import org.scalaquery.ql.basic.BasicDriver.Implicit._
 import org.scalaquery.ql.{Join, SimpleFunction, Query}
 
+import utils.QuartzScheduler
+
 object Global extends GlobalSettings {
 
   override def onStart(app: Application) {
     Logger.info("Application has started")
     
     implicit val ipapp = app
+
     // Clear out any jobs from a previous run...
     val db = Database.forDataSource(DB.getDataSource())
     db.withSession
     {
         ( for ( r <- controllers.CriticalMassTables.Jobs ) yield r ).mutate( _.delete )
     }
+
+    // Start the Quartz job scheduler
+    Logger.info( "Starting the Quartz scheduler" )
+    QuartzScheduler.start()
+    //QuartzScheduler schedule("job 1 ", foo) every (5 seconds)
+    //QuartzScheduler schedule("job 2 ", bar) at "0 0 3 * * ? *"
   }  
   
   override def onStop(app: Application) {
-    Logger.info("Application shutdown...")
+    // Stopping the Quartz scheduler
+    Logger.info( "Stopping the Quartz scheduler" )
+    QuartzScheduler.stop()
+    Logger.info( "Application shutdown..." )
   }  
     
 }
--- playsite/project/Build.scala
+++ playsite/project/Build.scala
@@ -11,13 +11,14 @@ object ApplicationBuild extends Build {
         "com.h2database" % "h2" % "1.3.167",
         "org.scalaquery" % "scalaquery_2.9.1" % "0.10.0-M1",
         "net.liftweb" % "lift-json_2.9.1" % "2.4",
-        "net.databinder" %% "dispatch-http" % "0.8.5"
+        "net.databinder" %% "dispatch-http" % "0.8.5",
+        "mchv" %% "play2-quartz" % "1.1"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
       // Add your own project settings here      
       
-        
+        resolvers += "Mariot Chauvin" at "http://mchv.me/repository"    
     )
 
 }
